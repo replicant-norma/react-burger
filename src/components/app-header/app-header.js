2ef9a6348619e15ecb7b9d8e980728b1c615e3ec
Replicant <example@example.com>
2021-08-28 23:02:04 +0300

    Исправлены замечания ревью 2

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -16,29 +16,31 @@ import {
 function AppHeader() {
     return (
         <header className={styles.header}>
-            <div className={clsx(styles.item, "pl-5 pr-5 mr-2")}>
-                <a href="#" className={clsx("text_type_main-default text_color_primary",
-                    styles.link)}>
-                    <BurgerIcon type="primary"/>
-                    <span>Конструктор</span>
-                </a>
-            </div>
-            <div className={clsx(styles.item, "pl-5 pr-5")}>
-                <a href="#" className={clsx("text_type_main-default text_color_inactive",
-                    styles.link)}>
-                    <ListIcon type="secondary"/>
-                    <span>Лента заказов</span>
-                </a>
-            </div>
-            <div className={clsx(styles.logo, "ml-30")}>
-                <Logo/>
-            </div>
-            <div className={clsx(styles.item, "pl-5 pr-5")}>
-                <a href="#" className={clsx("text_type_main-default text_color_inactive",
-                    styles.link)}>
-                    <ProfileIcon type="secondary"/>
-                    <span>Личный кабинет</span>
-                </a>
+            <div className={styles.container}>
+                <div className={clsx(styles.item, "pl-5 pr-5 mr-2")}>
+                    <a href="#" className={clsx("text_type_main-default text_color_primary",
+                        styles.link)}>
+                        <BurgerIcon type="primary"/>
+                        <span>Конструктор</span>
+                    </a>
+                </div>
+                <div className={clsx(styles.item, "pl-5 pr-5")}>
+                    <a href="#" className={clsx("text_type_main-default text_color_inactive",
+                        styles.link)}>
+                        <ListIcon type="secondary"/>
+                        <span>Лента заказов</span>
+                    </a>
+                </div>
+                <div className={clsx(styles.logo, "ml-30")}>
+                    <Logo/>
+                </div>
+                <div className={clsx(styles.item, "pl-5 pr-5")}>
+                    <a href="#" className={clsx("text_type_main-default text_color_inactive",
+                        styles.link)}>
+                        <ProfileIcon type="secondary"/>
+                        <span>Личный кабинет</span>
+                    </a>
+                </div>
             </div>
         </header>
 
